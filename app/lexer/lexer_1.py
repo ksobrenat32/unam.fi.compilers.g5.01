51d224721c029534af0924ee3e570a5f964ccd2c
lethalSopaper <luis.ugartechea.enp5.805@gmail.com>
2025-03-13 21:28:25 -0600

    report added in README.md

--- app/lexer/lexer_1.py
+++ app/lexer/lexer_1.py
@@ -1,19 +1,27 @@
 import streamlit as st
 from lexer.lexer import Lexer
 
+# Set the page layout to wide
+st.set_page_config(layout="wide")
+
 st.header("Lexer")
 
-source = st.text_area("Text to analyze")
+# Create two columns
+col1, col2 = st.columns(2)
 
-if st.button("Tokenize"):
-    lexer = Lexer(source)
-    st.subheader("Tokens")
+with col1:
+    source = st.text_area("Text to analyze", height=600)
 
-    # Format default dict
-    tokenized = ""
-    for key, value in lexer.tokenize().items():
-        tokenized += f"{key}: {value}\n"
-    st.success(tokenized)
+with col2:
+    if st.button("Tokenize"):
+        lexer = Lexer(source)
+        st.subheader("Tokens")
 
-    st.subheader("Token Count")
-    st.success(lexer.token_count())
+        # Format default dict
+        tokenized = lexer.tokenize()
+        for token_type, tokens in tokenized.items():
+            st.markdown(f"**• <u>{token_type.capitalize()}</u>**", unsafe_allow_html=True)
+            st.write(", ".join(tokens))
+
+        st.subheader("Token Count")
+        st.success(lexer.token_count())
