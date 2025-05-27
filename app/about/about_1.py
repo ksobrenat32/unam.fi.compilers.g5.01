c21c59d3e5d9040e86e4918d9a215d5ecb0ee31e
ksobrenat32 <ksobrenatural@tuta.io>
2025-05-27 13:10:03 -0600

    Delete all unused and change readme to show changes

--- app/about/about_1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.header("About")
-
-st.write("This is the beta version of the C-Lang Compiler.")
-st.write("In this version, you can classify inputs into tokens using lexical analysis, which allows us to generate the inputs our compiler will have for further analysis.")
